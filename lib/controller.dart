711e7dca06e9f61e0520c91602461d6ef9722e96
saltyfishie-fedora <teugene888@gmail.com>
2023-02-19 16:24:42 +0800

    fixed exactly same datetime insertion error

--- lib/controller.dart
+++ lib/controller.dart
@@ -45,7 +45,7 @@ class MainController extends StateXController {
     final dbCategories = res.last;
 
     for (final data in dbExpense) {
-      _expenseDataAdd(
+      _addExpenseToModel(
         Expense(
           datetime: DateTime.parse(data["datetime"].toString()),
           amount: data["amount"] as int,
@@ -63,6 +63,37 @@ class MainController extends StateXController {
   }
 
   Future<void> addExpense(Expense expense) async {
+    /// Checks if current expense's datatime exists in the database;
+    /// if it exists add 1 microsecond to the current expense's datetime.
+    ///
+    /// This was done because the datetime string was used as the primary key.
+    /// As such 2 exactly same datetime would cause an database insertion error.
+    while (true) {
+      final check = await _database.query(
+        _model.expenseTable,
+        where: "datetime = '${expense.datetime}'",
+      );
+
+      if (check.isEmpty) {
+        break;
+      }
+
+      final datetime = expense.datetime;
+      expense = expense.copyWith(
+        datetime: DateTime(
+          datetime.year,
+          datetime.month,
+          datetime.day,
+          datetime.hour,
+          datetime.minute,
+          datetime.second,
+          datetime.millisecond,
+          datetime.microsecond + 1,
+        ),
+      );
+    }
+
+    /// Actualy insert into the database after the above check
     _database.insert(_model.expenseTable, {
       "datetime": "${expense.datetime}",
       "amount": expense.amount,
@@ -70,13 +101,15 @@ class MainController extends StateXController {
       "category": expense.category,
     });
 
-    final query = await _database.query(
+    /// Check the inserted expense exist in the database
+    final insert = await _database.query(
       _model.expenseTable,
       where: "datetime = '${expense.datetime}'",
     );
 
-    for (final data in query) {
-      _expenseDataAdd(
+    /// Copy the database's expense into the model
+    for (final data in insert) {
+      _addExpenseToModel(
         Expense(
           datetime: DateTime.parse(data["datetime"].toString()),
           amount: data["amount"] as int,
@@ -91,7 +124,8 @@ class MainController extends StateXController {
     return _model.expenseData[index];
   }
 
-  void _expenseDataAdd(Expense expense) {
+  void _addExpenseToModel(Expense expense) {
+    // Create a new daily section if none exists
     if (_model.expenseData.isEmpty) {
       _model.expenseData.add([expense]);
       return;
@@ -101,6 +135,8 @@ class MainController extends StateXController {
       final sectionDate = _model.expenseData[i].first.datetime;
       final current = expense.datetime;
 
+      // Add to existing day section else
+      // Create a new day section if section does not exist
       if (current.year == sectionDate.year &&
           current.month == sectionDate.month &&
           current.day == sectionDate.day) {
@@ -112,10 +148,12 @@ class MainController extends StateXController {
       }
     }
 
+    // Sort daily sections
     _model.expenseData.sort(
       (a, b) => b.first.datetime.compareTo(a.first.datetime),
     );
 
+    // Sort entries in daily sections
     for (final data in _model.expenseData) {
       data.sort((a, b) => b.datetime.compareTo(a.datetime));
     }
@@ -150,4 +188,23 @@ class Expense {
   final int amount;
   final String title;
   final String category;
+
+  Expense copyWith({
+    DateTime? datetime,
+    int? amount,
+    String? title,
+    String? category,
+  }) {
+    datetime ??= this.datetime;
+    amount ??= this.amount;
+    title ??= this.title;
+    category ??= this.category;
+
+    return Expense(
+      datetime: datetime,
+      amount: amount,
+      title: title,
+      category: category,
+    );
+  }
 }
